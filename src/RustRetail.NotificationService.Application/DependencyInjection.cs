d6d0438b8dc246255d3d1394df3c8da244d45316
Nguyễn Thái Hoà <hoa41300@gmail.com>
2025-06-18 15:08:54 +0700

    Setup Dependency Injection for Application Layer

---
+++ src/RustRetail.NotificationService.Application/DependencyInjection.cs
@@ -0,0 +1,66 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using RustRetail.SharedApplication.Behaviors.Event;
+using RustRetail.SharedApplication.Behaviors.Logging;
+using RustRetail.SharedApplication.Behaviors.Validation;
+using RustRetail.SharedKernel.Domain.Events.Domain;
+
+namespace RustRetail.NotificationService.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(
+            this IServiceCollection services)
+        {
+            services.ConfigureMediatR()
+                .AddRequestLoggingBehavior()
+                .AddRequestValidationBehavior()
+                .AddDomainEventDispatcher();
+
+            return services;
+        }
+
+        private static IServiceCollection ConfigureMediatR(
+            this IServiceCollection services)
+        {
+            services.AddMediatR(config =>
+            {
+                config.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+            });
+
+            return services;
+        }
+
+        private static IServiceCollection AddRequestLoggingBehavior(
+            this IServiceCollection services)
+        {
+            services.AddScoped(
+                typeof(IPipelineBehavior<,>),
+                typeof(LoggingBehavior<,>));
+
+            return services;
+        }
+
+        private static IServiceCollection AddRequestValidationBehavior(
+            this IServiceCollection services)
+        {
+            // Validation behavior
+            services.AddScoped(
+                typeof(IPipelineBehavior<,>),
+                typeof(ValidationBehavior<,>));
+
+            // Validators
+            services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly);
+
+            return services;
+        }
+
+        private static IServiceCollection AddDomainEventDispatcher(
+            this IServiceCollection services)
+        {
+            services.AddScoped<IDomainEventDispatcher, MediatRDomainEventDispatcher>();
+            return services;
+        }
+    }
+}

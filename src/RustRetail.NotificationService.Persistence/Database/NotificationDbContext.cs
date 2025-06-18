11cfe85fac41a96fef01d753ca307348807277ba
Nguyễn Thái Hoà <hoa41300@gmail.com>
2025-06-18 15:04:34 +0700

    Add Notification DbContext

---
+++ src/RustRetail.NotificationService.Persistence/Database/NotificationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RustRetail.NotificationService.Domain.Entities;
+using RustRetail.SharedKernel.Domain.Models;
+using RustRetail.SharedPersistence.Abstraction;
+
+namespace RustRetail.NotificationService.Persistence.Database
+{
+    public class NotificationDbContext : DbContext, IHasOutboxMessage
+    {
+        public DbSet<Notification> Notifications { get; set; }
+        public DbSet<NotificationRecipient> NotificationRecipients { get; set; }
+        public DbSet<NotificationTemplate> NotificationTemplates { get; set; }
+        public DbSet<UserContactInfo> UserContactInfos { get; set; }
+        public DbSet<UserNotificationSetting> UserNotificationSettings { get; set; }
+        public DbSet<OutboxMessage> OutboxMessage { get; set; }
+
+        public NotificationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<OutboxMessage>().ToTable("OutboxMessages");
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(NotificationDbContext).Assembly);
+        }
+    }
+}
---
+++ src/RustRetail.NotificationService.Persistence/DependencyInjection.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RustRetail.NotificationService.Persistence.Database;
+using RustRetail.SharedPersistence.Interceptors;
+using Microsoft.EntityFrameworkCore;
+
+namespace RustRetail.NotificationService.Persistence
+{
+    public static class DependencyInjection
+    {
+        const string ConnectionStringName = "NotificationDatabase";
+
+        public static IServiceCollection AddPersistence(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddInterceptors()
+                .AddDbContext(configuration);
+
+            return services;
+        }
+
+        private static IServiceCollection AddDbContext(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<NotificationDbContext>((serviceProvider, options) =>
+            {
+                var interceptor = serviceProvider.GetRequiredService<DomainEventHandlingInterceptor>();
+                options.UseNpgsql(configuration.GetConnectionString(ConnectionStringName));
+                options.AddInterceptors(interceptor);
+            });
+
+            return services;
+        }
+
+        private static IServiceCollection AddInterceptors(
+            this IServiceCollection services)
+        {
+            services.AddScoped<DomainEventHandlingInterceptor>();
+
+            return services;
+        }
+    }
+}

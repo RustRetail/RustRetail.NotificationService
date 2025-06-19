a26bfe6c2e92978777dd1f553ede283b5c26a0fc
Nguyễn Thái Hòa <hoa41300@gmail.com>
2025-06-20 04:23:20 +0700

    Add Integration Event in Contracts Layer

---
+++ src/RustRetail.NotificationService.Contracts/IntegrationEvents/IntegrationEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace RustRetail.NotificationService.Contracts.IntegrationEvents
+{
+    public abstract class IntegrationEvent
+    {
+        public Guid Id { get; set; }
+
+        public DateTime OccurredOn { get; set; }
+    }
+}
---
+++ src/RustRetail.NotificationService.Contracts/IntegrationEvents/User/UserRegisteredEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace RustRetail.NotificationService.Contracts.IntegrationEvents.User
+{
+    public sealed class UserRegisteredEvent : IntegrationEvent
+    {
+        public Guid UserId { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+}

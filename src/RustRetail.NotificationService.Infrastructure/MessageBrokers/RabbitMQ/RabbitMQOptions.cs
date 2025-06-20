73a1987c3eb6b3cd6eddb78148df933bd6551036
Nguyễn Thái Hòa <hoa41300@gmail.com>
2025-06-20 08:25:52 +0700

    Add Message Broker (Partially)
    Add message broker with MassTransit and RabbitMQ.

---
+++ src/RustRetail.NotificationService.Infrastructure/MessageBrokers/RabbitMQ/RabbitMQOptions.cs
@@ -0,0 +1,14 @@
+﻿namespace RustRetail.NotificationService.Infrastructure.MessageBrokers.RabbitMQ
+{
+    public class RabbitMQOptions
+    {
+        public const string SectionName = "RabbitMQ";
+
+        public string Host { get; set; } = "localhost";
+        public string VirtualHost { get; set; } = "/";
+        public int Port { get; set; } = 5672;
+        public string UserName { get; set; } = "guest";
+        public string Password { get; set; } = "guest";
+        public string ExchangeName { get; set; } = "RustRetail.NotificationService";
+    }
+}
---
+++ src/RustRetail.NotificationService.Infrastructure/MessageBrokers/RabbitMQ/RabbitMQServiceCollectionExtensions.cs
@@ -0,0 +1,32 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RustRetail.NotificationService.Infrastructure.MessageBrokers.RabbitMQ
+{
+    internal static class RabbitMQServiceCollectionExtensions
+    {
+        internal static IServiceCollection AddRabbitMQ(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddMassTransit(x =>
+            {
+                x.SetKebabCaseEndpointNameFormatter();
+                //x.UsingRabbitMq((context, cfg) =>
+                //{
+                //    var options = configuration.GetSection(RabbitMQOptions.SectionName).Get<RabbitMQOptions>();
+                //    ArgumentNullException.ThrowIfNull(options, nameof(options));
+                //    cfg.Host(options.Host, options.VirtualHost, h =>
+                //    {
+                //        h.Username(options.UserName);
+                //        h.Password(options.Password);
+                //    });
+                //    cfg.ConfigureEndpoints(context);
+                //});
+            });
+
+            return services;
+        }
+    }
+}
